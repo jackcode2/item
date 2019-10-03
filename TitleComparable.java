8db836b52eac28af3099509ddc7d6e0030acf423
jackcode2 <806510870@qq.com>
2019-10-03 23:20:00 +0800

    书籍作者处理

---
+++ TitleComparable.java
@@ -0,0 +1,7 @@
+import java.util.Comparator;
+public class TitleComparable implements Comparator<Book>{
+    @Override
+    public int compare(Book o1,Book o2) {
+        return o1.title.compareTo(o2.title);
+    }
+}
